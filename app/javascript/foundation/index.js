daeb42b5ce1aeabd61ff292d71e7d3f6312ea292
haamityay <hamityay@hotmail.com>
2021-01-11 22:53:16 +0300

    Add foundation

---
+++ app/javascript/foundation/index.js
@@ -0,0 +1,10 @@
+import $ from 'jquery'
+import 'foundation-sites'
+import 'motion-ui'
+import './foundation_and_overrides.scss'
+
+
+$(document).ready(function () {
+  $(document).foundation();
+  console.log("Zurb Foundation loaded");
+})
